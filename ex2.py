b1207defe3aeccc14486535f0c87f455d168cccd
Gal Koren <koren.gal@gmail.com>
2025-12-09 16:29:24 +0200

    output 13

--- ex2.py
+++ ex2.py
@@ -1,20 +1,36 @@
 import sys
+from collections import Counter
 from file_utils import read_file_lines, write_outputs_to_file, Output
 
 VOCAB_SIZE = 300000
 
 
-def compute_word_stats(lines: list[str]) -> tuple[dict[str, int], int]:
-    word_counts = {}
-    total_events = 0
-    for line in lines:
-        words = line.split()
-        for word in words:
-            word_counts[word] = word_counts.get(word, 0) + 1
-            total_events += 1
-    
-    return word_counts, total_events
+def make_dict(words: list[str]) -> dict[str, int]:
+    return dict(Counter(words))
 
+
+def split_to_words(lines: list[str]) -> list[str]:
+    words = []
+    for line in lines:
+        line_words = line.split()
+        for word in line_words:
+            words.append(word)    
+    return words
+
+class Sample:
+    def __init__(self, events: list[str]):
+        self.events = events
+        self.size = len(events)
+        self.dict: dict[str, int] = make_dict(events)
+
+    def unique_events_count(self):
+        return len(self.dict.keys())
+    
+    def count_word(self, word: str) -> int:
+        return self.dict.get(word, 0)
+    
+    def mle(self, word: str) -> int:
+        return self.dict.get(word, 0) / self.size
 
 # Parse command line arguments
 if len(sys.argv) != 5:
@@ -38,10 +54,23 @@ outputs.append(Output(5, [VOCAB_SIZE]))
 outputs.append(Output(6, [1/VOCAB_SIZE]))
 
 
-lines = read_file_lines(dev_file_name)
-dev_dict, num_of_events = compute_word_stats(lines)
+dev_lines = read_file_lines(dev_file_name)
+dev_words = split_to_words(dev_lines)
 
-outputs.append(Output(7, [str(num_of_events)]))
+num_of_90_percent = round(len(dev_words) * 0.9)
+
+training_set = Sample(dev_words[:num_of_90_percent])
+validation_set = Sample(dev_words[num_of_90_percent:])
+
+outputs.append(Output(7, [str(len(dev_words))]))
+outputs.append(Output(8, [str(validation_set.size)]))
+outputs.append(Output(9, [str(training_set.size)]))
+
+
+outputs.append(Output(10, [str(training_set.unique_events_count())]))
+outputs.append(Output(11, [str(training_set.count_word(input_word))]))
+outputs.append(Output(12, [str(training_set.mle(input_word))]))
+outputs.append(Output(13, [str(training_set.mle('unseen-word'))]))
 
 
 write_outputs_to_file(output_file_name, outputs)
